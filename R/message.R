ba19ba9630d1729cb1882fa4690f19c793c242da
Florian Privé <florian.prive.21@gmail.com>
2019-10-08 21:44:18 +0200

    messages

---
+++ R/message.R
@@ -0,0 +1,31 @@
+################################################################################
+
+#' Easy messages
+#'
+#' @inheritDotParams sprintf
+#'
+#' @export
+#'
+#' @examples
+#' printf("My name is %s.", "Florian")
+printf <- function(...) cat(sprintf(...))
+
+################################################################################
+
+#' @rdname printf
+#' @export
+message2 <- function(...) message(sprintf(...))
+
+################################################################################
+
+#' @rdname printf
+#' @export
+warning2 <- function(...) warning(sprintf(...), call. = FALSE)
+
+################################################################################
+
+#' @rdname printf
+#' @export
+stop2 <- function(...) stop(sprintf(...), call. = FALSE)
+
+################################################################################
